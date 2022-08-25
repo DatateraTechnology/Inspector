3a0bd2ee0d99f0d5499e691a73fdd86c0ea549b0
Tugce <52296491+TugceOzdeger@users.noreply.github.com>
2022-08-25 20:12:46 +0300

    Update app.py

--- app.py
+++ app.py
@@ -5,20 +5,20 @@ from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 import re
 import pandas as pd
+import matplotlib.pyplot as plt
+from termcolor import colored
+import klib
 import tensorflow as tf
 import json, random, numpy as np
-import matplotlib.pyplot as plt
 import urllib.request, json, seaborn, os, uuid
-import pandas as pd
 from typing import List
-from presidio_analyzer import AnalyzerEngine, EntityRecognizer, Pattern, RecognizerResult
+from presidio_analyzer import AnalyzerEngine, RecognizerResult
 from presidio_analyzer.nlp_engine import NlpArtifacts
 from presidio_anonymizer import AnonymizerEngine
 from presidio_analyzer import AnalyzerEngine
 from typing import List
-from presidio_analyzer import AnalyzerEngine, EntityRecognizer, RecognizerResult
+from presidio_analyzer import AnalyzerEngine, RecognizerResult
 from presidio_analyzer.nlp_engine import NlpArtifacts
-from transformers import pipeline
 import os
 
 app = Flask(__name__)
@@ -217,7 +217,7 @@ def sensitive_data_cnn():
     plt.ylabel('Expected')
 
   model_json = model.to_json()
-  local_file_name = "Model_" + str(uuid.uuid4()) + ".json"
+  local_file_name = "Model_" + str(uuid.uuid4()) + ".png"
   blob = BlobClient.from_connection_string("DefaultEndpointsProtocol=https;AccountName=datateraalpha;AccountKey=W890/aL1FprdvAsAV4xXpOof1BZQm5Ujb044t8s2XaHFeA0QBYlffI+KYG72uQCg6Ly8SNkeRki8cOwma4co9A==;EndpointSuffix=core.windows.net", container_name="beta", blob_name=local_file_name)
   blob.upload_blob(model_json)
 
@@ -386,3 +386,144 @@ def sensitive_data_pre():
     print(predict_fn(data,AnalyzerEngine())[1])
 
     return jsonify(f"Anonymized/Nonanonymized Data:{datax}")
+
+@app.route("/beta/qualitydata", methods=["GET"], endpoint='quality_data')
+def quality_data():
+      
+  plt.rcParams["figure.figsize"] = (10,6)
+  pd.set_option('max_colwidth',200)
+  pd.set_option('display.max_rows', 1000)
+  pd.set_option('display.max_columns', 200)
+  pd.set_option('display.float_format', lambda x: '%.3f' % x)
+
+  """# Loading Dataset"""
+  url=request.args.get('Quality_Data')
+  df=pd.read_csv(url)
+  
+  print(colored("Shape:", attrs=['bold']), df.shape,'\n', 
+      colored('*'*100, 'red', attrs = ['bold']),
+      colored("\nInfo:\n", attrs = ['bold']), sep = '')
+  print(df.info(), '\n', 
+      colored('*'*100, 'red', attrs = ['bold']), sep = '')
+  print(colored("Number of Uniques:\n", attrs = ['bold']), df.nunique(),'\n',
+      colored('*'*100, 'red', attrs = ['bold']), sep = '')
+  print(colored("All Columns:", attrs = ['bold']), list(df.columns),'\n', 
+      colored('*'*100, 'red', attrs = ['bold']), sep = '')
+
+  df.columns = df.columns.str.lower().str.replace('&', '_').str.replace(' ', '_')
+  print(colored("Columns after rename:", attrs = ['bold']), list(df.columns),'\n',
+      colored('*'*100, 'red', attrs = ['bold']), sep = '')  
+  print(colored("Descriptive Statistics \n", attrs = ['bold']), df.describe().round(2),'\n',
+      colored('*'*100, 'red', attrs = ['bold']), sep = '') # Gives a statstical breakdown of the data.
+  print(colored("Descriptive Statistics (Categorical Columns) \n", attrs = ['bold']), df.describe(include = object).T,'\n',
+      colored('*'*100, 'red', attrs = ['bold']), sep = '') # Gives a statstical breakdown of the data.
+
+  """# Functions for Missing Values, Multicolinearity and Duplicated Values"""
+  def missing_values():
+   missing_number = df.isnull().sum().sort_values(ascending = False)
+   missing_percent = (df.isnull().sum() / df.isnull().count()).sort_values(ascending = False)
+   missing_values = pd.concat([missing_number, missing_percent], axis = 1, keys = ['Missing_Number', 'Missing_Percent'])
+   missing_values[missing_values['Missing_Number'] > 0]
+   return missing_values[missing_values['Missing_Number'] > 0]
+
+  def multicolinearity_control():
+    feature = []
+    collinear = []
+    for col in df.corr().columns:
+      for i in df.corr().index:
+        if (abs(df.corr()[col][i]) > .9 and abs(df.corr()[col][i]) < 1):
+          feature.append(col)
+          collinear.append(i)
+          print(colored(f"Multicolinearity alert in between:{col} - {i}", 
+                                  "red", attrs = ['bold']), df.shape,'\n',
+                                  colored('*'*100, 'red', attrs = ['bold']), sep = '')
+
+    if len(collinear)==0:
+        print("No Multicoliearity, Correlation between collumns is NOT over %90")
+
+  def duplicate_values():
+    print(colored("Duplicate check...", attrs = ['bold']), sep = '')
+    print("There are", df.duplicated(subset = None, keep = 'first').sum(), "duplicated observations in the dataset.")
+    duplicate_values = df.duplicated(subset = None, keep = 'first').sum()
+    #if duplicate_values > 0:
+        #df.drop_duplicates(keep = 'first', inplace = True)
+        #print(duplicate_values, colored(" Duplicates were dropped!"),'\n',
+              #colored('*'*100, 'red', attrs = ['bold']), sep = '')
+      #     else:
+      #         print(colored("There are no duplicates"),'\n',
+      #               colored('*'*100, 'red', attrs = ['bold']), sep = '')     
+            
+      # def drop_columns(df, drop_columns):
+      #     if drop_columns != []:
+      #         df.drop(drop_columns, axis = 1, inplace = True)
+      #         print(drop_columns, 'were dropped')
+      #     else:
+      #         print(colored('We will now check the missing values and if necessary, the related columns will be dropped!', attrs = ['bold']),'\n',
+      #               colored('*'*100, 'red', attrs = ['bold']), sep = '')
+
+  """# Missing Values, Multicolienaity and Duplicated Values"""
+
+  multicolinearity_control()
+
+  duplicate_values()
+
+  missing_values()
+
+  df.sample(2)
+
+  klib.missingval_plot(df)
+  
+  """# DATA QUALITY FUNCTION"""
+
+  def Quality_Check():
+   print("*****************************************DATA SUMMARY***********************************************")
+   print(quality_data())
+   print("****************************************MISSING VALUES**********************************************")
+   print(missing_values())
+   print(colored("Shape:", attrs=['bold']), df.shape,'\n', 
+          colored('*'*100, 'red', attrs = ['bold']),
+          colored("\nInfo:\n", attrs = ['bold']), sep = '')
+   print("***************************************DUPLICATED VALUES********************************************")
+   print(duplicate_values())
+   print(colored("Shape:", attrs=['bold']), df.shape,'\n', 
+          colored('*'*100, 'red', attrs = ['bold']),
+          colored("\nInfo:\n", attrs = ['bold']), sep = '')
+   print("*************************************MULTICOLINEARITY CHECK*****************************************")
+   multicolinearity_control()
+
+   Quality_Check()
+
+   """# KPI Function"""
+
+  def KPI():
+   print("**********************************NUMBER OF COLUMNS AND ROWS****************************************")
+   print("There are", df.shape[0], "rows", df.shape[1],"columns and",df.shape[0]*df.shape[1],"entries in this dataset")
+   print()
+   print("****************************************MISSING VALUES**********************************************")
+   print("Overall percentage of missing values is %", missing_values().mean()[1]*100)
+   print("")
+   print("***************************************DUPLICATED VALUES********************************************")
+   print("There are",df.duplicated(subset = None, keep = 'first').sum(),"duplicated values.","Overall percentage is %", (df.duplicated(subset = None, keep = 'first').sum()/len(df))*100)
+   print("")
+   print("*************************************MULTICOLINEARITY CHECK*****************************************")
+   multicolinearity_control()
+   print("")
+   print()
+   print("***************************************OVERALL DATA QUALITY*****************************************")
+ 
+   KPI()
+
+  """# KPI Assesement
+    High Quality Data Criteria
+
+    1.   Overall Missing Value percentage less than %5 and,
+    2.   Overall Duplicated Value percentage less than %2 and,
+    3.   No Multicolinearity (Correlation between columns NOT higher than %90) and,
+    4.   Overall Erroneous Data percentage is less than %2.
+
+  """
+
+  if (missing_values().mean()[1] <.05) and (df.duplicated(subset = None, keep = 'first').sum()/len(df)<.02):
+    return jsonify("HIGH QUALITY DATA")
+  else:
+    return jsonify("LOW QUALITY DATA")
